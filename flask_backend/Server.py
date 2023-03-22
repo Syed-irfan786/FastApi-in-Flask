be9a212272a958a63ca736543e0d1832d7709081
syed-irfan786 <syedirfan.bses19@iba-suk.edu.pk>
2023-03-22 16:39:07 +0500

    The error in react form resolve and data can be directly from Form to Database

--- flask_backend/Server.py
+++ flask_backend/Server.py
@@ -1,10 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from flask import Flask, request, jsonify
 import mysql.connector as mysql
 from fastapi import FastAPI
 from flask_cors import CORS
-
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+app = Flask(__name__)
+CORS(app)
 app = FastAPI()
+origins = [
+     "http://localhost",
+    "http://192.168.0.85:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+
 conn = mysql.connect(host="localhost", user="root", password="", database="test_fast_api")
 
 @app.get("/get-all-data")
@@ -33,12 +70,28 @@ def deleteDataById(id: int):
     conn.commit()
     return "Deleted successfully"
 
-
+class Item(BaseModel):
+    item_name: str
+    item_price: float
+    item_status: int
 
 @app.post("/post-data")
-def postdata(item_name:str , item_price:int, item_status:int ):
-        curs=conn.cursor(dictionary=True)
-        query = "Insert into items (`item_name`, `item_price`, `item_status`) values ('"+item_name+"',"+str(item_price)+","+str(item_status)+")"
-        curs.execute(query)
-        conn.commit()
-        return "Data successfully inserted into database"
+async def post_data(item:Item):
+    item_name = item.item_name
+    item_price = item.item_price
+    item_status = item.item_status
+    curs=conn.cursor(dictionary=True)
+    query = "Insert into items (`item_name`, `item_price`, `item_status`) values ('"+item_name+"',"+str(item_price)+","+str(item_status)+")"
+    curs.execute(query)
+    conn.commit()
+    return "Data successfully inserted into database"
+             
+
+@app.get("/delete-by-name/{name}")
+def deleteByName(name:str):
+    curs=conn.cursor(dictionary=True)
+    query="DELETE FROM items where item_name="+name
+    curs.execute(query)
+    conn.commit()
+    return "Deleted Successfully"
+
--- react_frontend/src/form1.js
+++ react_frontend/src/form1.js
@@ -2,61 +2,46 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function MyForm() {
-  const [formData, setFormData] = useState({});
+  const [itemName, setItemName] = useState('');
+  const [itemPrice, setItemPrice] = useState('');
+  const [itemStatus, setItemStatus] = useState('');
 
-  const handleSubmit = () => {
-    console.log(formData,8)
-    axios.post("http://127.0.0.1:8000/post-data", formData)  // 127.0.0.1:3000/post-data   127.0.0.1:8000/post-data
-      .then(response => {
-        console.log("hello testing");
-      })
-      .catch(error => {
-        console.log("hello errror", error);
-      });
-  };
-  
-  const handleChange = (event) => {
-    
-    setFormData({ ...formData, [event.target.name]: event.target.value });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post('http://127.0.0.1:8000/post-data', {
+      item_name: itemName,
+      item_price: itemPrice,
+      item_status: itemStatus
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error));
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Item Name:
-        <input type="text" name="name" onChange={handleChange} />
-      </label> <br/>
+        <input type="text" value={itemName} onChange={(e) => setItemName(e.target.value)} />
+      </label>
+      <br />
       <label>
         Item Price:
-        <input type="text" name="price" onChange={handleChange} />
-      </label><br/>
+        <input type="number" value={itemPrice} onChange={(e) => setItemPrice(e.target.value)} />
+      </label>
+      <br />
       <label>
         Item Status:
-        <input type="text" name="status" onChange={handleChange} />
-      </label> <br/>
+        <input type="number" value={itemStatus} onChange={(e) => setItemStatus(e.target.value)} />
+      </label>
+      <br />
       <button onClick={handleSubmit}>Submit</button>
     </form>
   );
 }
 
 export default MyForm;
-
-
-
-
-
-  //   <div>
-  //   <form action="/post-data" method = "POST" onSubmit={handleSubmit}>
-  //  <p>Item name <input type = "text" name = "item_name" onChange={handleChange} /></p>
-  //  <p>Price <input type = "integer" name = "price" /></p>
-  //  <p>Status <input type = "text" name = "item_status" /></p>
-  //  <p><input type = "submit" value = "Send" /></p>
-  // </form>
-  // </div>
-
-    
-
-
-
-
-
